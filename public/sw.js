17c73736729bf7dd243a3245904a5aff114d7e45
soeltan zaki rizaldy <zcky1990@gmail.com>
2026-04-13 13:20:17 +0700

    fixing pwa

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'warung-zavi-v1';
+const CACHE_NAME = 'warung-zavi-v2';
 const ASSETS = [
   './',
   'index.html',
@@ -8,9 +8,24 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
